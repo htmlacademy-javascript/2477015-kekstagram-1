c435a3016daf07464b9e80cd2988704ad91aa713
Elizaveta <elizaveta.rumyanceva@mail.ru>
2024-01-07 00:49:48 +0300

    fix: корректировка решений задач в functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,34 +1,33 @@
-"use strict";
-
 // ТЗ №1: Функция для проверки, является ли строка палиндромом
+const isPalindrome = (value) => {
+  const string = value.replaceAll(' ', '').toLowerCase();
+  const result = [...string].reverse().join('');
 
-function checkPalindrome(value) {
-  // Через окно ввода: let value = prompt("Укажите значение");
-  value = value.replaceAll(" ", "").toLowerCase();
-  let result = [...value].reverse().join("");
-  value == result
-    ? console.log("Это палиндром")
-    : console.log("Это не палиндром");
-}
-checkPalindrome("топот"); // true
-checkPalindrome("ДовОд"); // true
-checkPalindrome("Кекс"); // false
-checkPalindrome(" Лёша на полке клопа нашёл  "); // true
+  return string === result;
+};
 
-// ТЗ №2: Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
+isPalindrome('топот'); // true
+isPalindrome('ДовОд'); // true
+isPalindrome('Кекс'); // false
+isPalindrome(' Лёша на полке клопа нашёл  '); // true
 
-function checkNumber(value) {
-  let result = Number.parseInt(value.toString().replaceAll(/[^0-9]/g, ""));
-  return console.log(result);
-}
-checkNumber("2023 год"); // 2023
-checkNumber("ECMAScript 2022"); // 2022
-checkNumber("1 кефир, 0.5 батона"); // 105
-checkNumber("агент 007"); // 7
-checkNumber("а я томат"); // NaN
-checkNumber(2023); // 2023
-checkNumber(-1); // 1
-checkNumber(1.5); //15
+// ТЗ №2: Функция, которая принимает строку, извлекает в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN
+const extractNumber = (value) => {
+  const result = Number
+    .parseInt(value.toString()
+      .replaceAll(/[^0-9]/g, ''), 10);
+
+  return result;
+};
+
+extractNumber('2023 год'); // 2023
+extractNumber('ECMAScript 2022'); // 2022
+extractNumber('1 кефир, 0.5 батона'); // 105
+extractNumber('агент 007'); // 7
+extractNumber('а я томат'); // NaN
+extractNumber(2023); // 2023
+extractNumber(-1); // 1
+extractNumber(1.5); //15
 
 /*
 ТЗ №3: Функция, которая:
@@ -41,44 +40,47 @@ checkNumber(1.5); //15
 */
 
 // Через padStart()
-function toAddSymbPadStart(value, maxLength, newValue) {
-  let newString = value.toString().padStart(maxLength, newValue);
-  console.log(newString);
-}
-toAddSymbPadStart("1", 2, "0"); // '01'
-toAddSymbPadStart("1", 4, "0"); // '0001'
-toAddSymbPadStart("q", 4, "werty"); // 'werq'
-toAddSymbPadStart("q", 4, "we"); // 'wewq' (????)
-toAddSymbPadStart("qwerty", 4, "0"); // 'qwerty'
-toAddSymbPadStart("qwerty", 8, 0); // '00qwerty'
+const toAddSymbPadStart = (value, maxLength, newValue) => {
+  const newString = value.padStart(maxLength, newValue);
+
+  return newString;
+};
+
+toAddSymbPadStart('1', 2, '0'); // '01'
+toAddSymbPadStart('1', 4, '0'); // '0001'
+toAddSymbPadStart('q', 4, 'werty'); // 'werq'
+toAddSymbPadStart('q', 4, 'we'); // 'wewq' (????)
+toAddSymbPadStart('qwerty', 4, '0'); // 'qwerty'
+toAddSymbPadStart('qwerty', 8, 0); // '00qwerty'
 
 // Через slice() и repeat()
-let toAddSymbols = function (value, maxLength, addValue) {
-  let minLength = maxLength - value.length;
-  if (minLength <= 0) {
-    return value;
-  }
-  return (
+const toAddSymbols = (value, maxLength, addValue) => {
+  const minLength = maxLength - value.length;
+
+  const result = (minLength <= 0) ? value :
     addValue.slice(0, minLength % addValue.length) +
     addValue.repeat(minLength / addValue.length) +
-    value
-  );
+    value;
+
+  return result;
 };
-console.log(toAddSymbols("1", 2, "0")); // '01'
-console.log(toAddSymbols("1", 4, "0")); // '0001'
-console.log(toAddSymbols("q", 4, "werty")); // 'werq'
-console.log(toAddSymbols("q", 4, "we")); // 'wweq'
-console.log(toAddSymbols("qwerty", 4, "0")); // 'qwerty'
+
+toAddSymbols('1', 2, '0'); // '01'
+toAddSymbols('1', 4, '0'); // '0001'
+toAddSymbols('q', 4, 'werty'); // 'werq'
+toAddSymbols('q', 4, 'we'); // 'wweq'
+toAddSymbols('qwerty', 4, '0'); // 'qwerty'
 
 // ТЗ №3: Функция для проверки длины строки
+const canLength = (value, maxLength) => {
+  const result = value.length <= maxLength;
 
-function checkLength(value, maxLength) {
-  // Через окно ввода: let value = prompt("Укажите значение");
-  let result = value.length <= maxLength ? true : false;
-  console.log(result);
-}
-checkLength("проверяемая строка", 20); // true
-checkLength("проверяемая строка", 18); // true
-checkLength("проверяемая строка", 10); // false
-checkLength("проверяемая строка", 5); // false
-checkLength("проверяемая строка", 19); // false
+  return result;
+};
+
+canLength('проверяемая строка', 20); // true
+canLength('проверяемая строка', 18); // true
+canLength('проверяемая строка', 10);// false
+canLength('проверяемая строка', 5); // false
+canLength('проверяемая строка', 19); // false
+

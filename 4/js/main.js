b3b7bc7c801f74778b6a3ec3dfdc6fa530d92039
Keks <keks@htmlacademy.ru>
2024-01-23 23:11:15 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,11 +1,7 @@
 const ALL_DESCRIPTION_PHOTOS = 25;
-
 const AVATARS_COMENTATORS = 6;
-
 const MIN_LIKES = 15;
-
 const MAX_LIKES = 200;
-
 const ALL_DESCRIPTIONS = [
   'Как быстро летит время!',
   'Happiness can be found, even in the darkest of times, if one only remembers to turn on the light. — Albus Dumbledore',
@@ -17,7 +13,6 @@ const ALL_DESCRIPTIONS = [
   'Как жаль, что солнце в Санкт-Петербурге такое редкое явление!',
   'Нас и здесь неплохо кормят!',
 ];
-
 const ALL_COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -26,7 +21,6 @@ const ALL_COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
-
 const NAMES_COMENTATORS = [
   'Natalia',
   'Elizaveta',
@@ -44,9 +38,7 @@ const NAMES_COMENTATORS = [
   'Olga',
   'Sofia',
 ];
-
-const NUMBER_COMMENTS = 15;
-
+const MAX_COMMENTS = 15;
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -56,51 +48,26 @@ const getRandomInteger = (a, b) => {
   return Math.floor(result);
 };
 
-
 const getRandomArrElement = (newArr) =>
   newArr[getRandomInteger(0, newArr.length - 1)];
 
-
-function createIdGenerator () {
-  let lastGeneratedId = 0;
-
-  return function () {
-    lastGeneratedId += 1;
-
-    return lastGeneratedId;
-  };
-}
-
-const generatePhotoId = createIdGenerator();
-const generateCommentId = createIdGenerator();
-const generatePhotoUrl = createIdGenerator();
-
-
-const createMessage = () =>
-  Array.from({length: 1}, () =>
-    getRandomArrElement (ALL_COMMENTS));
-
-
-const createComment = () => ({
-  id: generateCommentId(),
+const createComment = (id) => ({
+  id,
   avatar: `img/avatar-${getRandomInteger(1, AVATARS_COMENTATORS)}.svg`,
-  message: createMessage(),
+  message:getRandomArrElement(ALL_COMMENTS),
   name: getRandomArrElement(NAMES_COMENTATORS),
 });
 
-
-const createDescriptionPhoto = () => ({
-  id: generatePhotoId(),
-  url: `photos/${generatePhotoUrl()}.jpg`,
+const createPhoto = (id) => ({
+  id,
+  url: `photos/${id}.jpg`,
   description: getRandomArrElement(ALL_DESCRIPTIONS),
   likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
-  comments: Array.from({length: getRandomInteger(1, NUMBER_COMMENTS)}, createComment),
+  comments: Array.from({length: getRandomInteger(1, MAX_COMMENTS)}, (_, index) => createComment (index + 1)),
 });
 
-
-const allDescriptionPhoto = () =>
+const createGallery = () =>
   Array
-    .from({length: ALL_DESCRIPTION_PHOTOS})
-    .map((_, index) => createDescriptionPhoto (index + 1));
+    .from({length: ALL_DESCRIPTION_PHOTOS}, (_, index) => createPhoto (index + 1));
 
-allDescriptionPhoto();
+createGallery();

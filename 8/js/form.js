620119ca20c5ed4e533c1a119bd3647b62e6cadc
Keks <keks@htmlacademy.ru>
2024-03-10 15:07:13 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/form.js
+++ 8/js/form.js
@@ -1,51 +1,42 @@
 import {isEscapeKey} from './util.js';
-import {getValidForm, pristineForm} from './form-validate.js';
+import {pristineForm} from './form-validate.js';
 
 const imgUploadOverlay = document.querySelector('.img-upload__overlay');
 const imgUploadCancel = document.querySelector('.img-upload__cancel');
-
 const imgUploadForm = document.querySelector('.img-upload__form');
 const imgUploadInput = imgUploadForm.querySelector('.img-upload__input');
 const textHashtags = imgUploadForm.querySelector('.text__hashtags');
 const textDescription = imgUploadForm.querySelector('.text__description');
 
-export const openForm = () => {
+export const openForm =
   imgUploadInput.addEventListener('change', () => {
     imgUploadOverlay.classList.remove('hidden');
     document.body.classList.add('modal-open');
     imgUploadCancel.addEventListener('click', closeForm);
     document.addEventListener('keydown', onFormKeydown);
-    getValidForm();
   });
-};
 
 function closeForm () {
   imgUploadForm.reset();
   pristineForm.reset();
-  textHashtags.value = '';
-  imgUploadInput.value = '';
   imgUploadOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onFormKeydown);
 }
 
+const onInputInFocus = () =>
+  document.activeElement === textHashtags ||
+  document.activeElement === textDescription;
+
 function onFormKeydown (evt) {
-  if (isEscapeKey(evt.key)) {
+  if (isEscapeKey(evt.key) && !onInputInFocus()) {
     evt.preventDefault();
     closeForm();
   }
 }
 
-imgUploadCancel.addEventListener('click', () => {
-  closeForm();
+imgUploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristineForm.validate();
 });
 
-const inputInFocus = (evt) => {
-  if (isEscapeKey(evt.key)) {
-    evt.preventDefault();
-    evt.stopPropagation();
-  }
-};
-
-textHashtags.addEventListener('keydown', inputInFocus);
-textDescription.addEventListener('keydown', inputInFocus);

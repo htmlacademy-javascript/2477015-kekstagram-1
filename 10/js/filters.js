2ba38a554cc033a5150ae23d35ff476c44a3853d
Keks <keks@htmlacademy.ru>
2024-03-25 17:59:28 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/filters.js
@@ -0,0 +1,113 @@
+const FILTERS = {
+  origin: {
+    filter: 'none',
+    unit: '',
+    min: 100,
+    max: 100,
+    step: 1,
+    start: 100,
+    connect: 'lower',
+  },
+  chrome: {
+    filter: 'grayscale',
+    unit: '',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    start: 1,
+    connect: 'lower',
+  },
+  sepia: {
+    filter: 'sepia',
+    unit: '',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    start: 1,
+    connect: 'lower',
+  },
+  marvin: {
+    filter: 'invert',
+    unit: '%',
+    min: 0,
+    max: 100,
+    step: 1,
+    start: 100,
+    connect: 'lower',
+  },
+  phobos: {
+    filter: 'blur',
+    unit: 'px',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    start: 3,
+    connect: 'lower',
+  },
+  heat: {
+    filter: 'brightness',
+    unit: '',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    start: 3,
+    connect: 'lower',
+  }
+};
+
+const imgUploadForm = document.querySelector('.img-upload__form');
+const imgUploadPreview = imgUploadForm.querySelector('.img-upload__preview');
+const imgUploadEffectLevel = imgUploadForm.querySelector('.img-upload__effect-level');
+const effectsList = imgUploadForm.querySelector('.effects__list');
+const effectLevelValue = imgUploadForm.querySelector('.effect-level__value');
+const effectLevelSlider = imgUploadForm.querySelector('.effect-level__slider');
+
+let activeFilter = FILTERS.origin;
+
+noUiSlider.create(effectLevelSlider, {
+  range: {
+    min: FILTERS.origin.min,
+    max: FILTERS.origin.max,
+  },
+  start:  FILTERS.origin.start,
+  step:  FILTERS.origin.step,
+  connect: FILTERS.origin.connect,
+});
+
+const setPictureEffect = () => {
+  effectLevelValue.value = effectLevelSlider.noUiSlider.get();
+  imgUploadPreview.style.filter =
+  `${activeFilter.filter}(${effectLevelValue.value}${activeFilter.unit})`;
+};
+
+const updateSliderOptions = ({ min, max, start, step, connect}) => {
+  effectLevelSlider.noUiSlider.updateOptions({
+    range: {
+      min,
+      max,
+    },
+    start,
+    step,
+    connect,
+  });
+};
+
+export const resetPictureEffect = () => {
+  activeFilter = FILTERS.origin;
+  imgUploadEffectLevel.classList.add('hidden');
+  imgUploadPreview.style.filter = '';
+  effectLevelValue.value = '';
+};
+
+effectsList.addEventListener('change', (evt) => {
+  activeFilter = FILTERS[evt.target.value] ?? FILTERS.origin;
+
+  if (activeFilter === FILTERS.origin) {
+    resetPictureEffect();
+  } else {
+    imgUploadEffectLevel.classList.remove('hidden');
+    updateSliderOptions(activeFilter);
+  }
+});
+
+effectLevelSlider.noUiSlider.on('update', setPictureEffect);
---
+++ 10/js/picture-scale.js
@@ -0,0 +1,39 @@
+const MIN_SCALE = 25;
+const MAX_SCALE = 100;
+const SCALE_STEP = 25;
+
+const imgUploadForm = document.querySelector('.img-upload__form');
+const smallerControlButton = imgUploadForm.querySelector('.scale__control--smaller');
+const biggerControlButton = imgUploadForm.querySelector('.scale__control--bigger');
+const scaleControlValue = imgUploadForm.querySelector('.scale__control--value');
+const imgUploadPreview = imgUploadForm.querySelector('.img-upload__preview');
+
+const getScaleValue = () => parseInt(scaleControlValue.value, 10);
+
+const setPictureScale = (value) => {
+  imgUploadPreview.style.transform = `scale(${value / 100})`;
+  scaleControlValue.value = `${value}%`;
+};
+
+const onSmallerControlButtonClick = () => {
+  if (getScaleValue() <= MIN_SCALE) {
+    return;
+  }
+
+  setPictureScale(Math.max(getScaleValue() - SCALE_STEP, MIN_SCALE));
+};
+
+const onBiggerControlButtonClick = () => {
+  if (getScaleValue() >= MAX_SCALE) {
+    return;
+  }
+
+  setPictureScale(Math.min((getScaleValue() + SCALE_STEP), MAX_SCALE));
+};
+
+export const resetScalePicture = () => {
+  setPictureScale(getScaleValue(MAX_SCALE));
+};
+
+smallerControlButton.addEventListener('click', onSmallerControlButtonClick);
+biggerControlButton.addEventListener('click', onBiggerControlButtonClick);
